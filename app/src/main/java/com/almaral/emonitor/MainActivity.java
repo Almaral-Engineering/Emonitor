16fb0acf690fda6e5c547e1a1ee1f85f760193ad
Jesus Almaral <almaral17@gmail.com>
2016-12-19 17:53:38 -0700

    Creates AsyncTask to download earthquake data

--- app/src/main/java/com/almaral/emonitor/MainActivity.java
+++ app/src/main/java/com/almaral/emonitor/MainActivity.java
@@ -3,15 +3,9 @@ package com.almaral.emonitor;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ListView;
-import android.widget.Toast;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
-import java.nio.charset.Charset;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
@@ -30,52 +24,12 @@ public class MainActivity extends AppCompatActivity {
 
         EqAdapter eqAdapter = new EqAdapter(this, R.layout.eq_list_item, eqList);
         earthquakeListView.setAdapter(eqAdapter);
-    }
 
-    private String downloadData(URL url) throws IOException {
-        String jsonResponse = "";
-        HttpURLConnection urlConnection = null;
-        InputStream inputStream = null;
-
+        DownloadEqsAsyncTask downloadEqsAsyncTask = new DownloadEqsAsyncTask();
         try {
-            urlConnection = (HttpURLConnection) url.openConnection();
-            urlConnection.setRequestMethod("GET");
-            urlConnection.setReadTimeout(10000 /* milliseconds */);
-            urlConnection.setConnectTimeout(15000 /* milliseconds */);
-            urlConnection.connect();
-
-            inputStream = urlConnection.getInputStream();
-            jsonResponse = readFromStream(inputStream);
-        } catch (IOException e) {
-            Toast.makeText(this, R.string.download_eartquakes_network_error,
-                    Toast.LENGTH_SHORT).show();
-        } finally {
-            if (urlConnection != null) {
-                urlConnection.disconnect();
-            }
-
-            if (inputStream != null) {
-                inputStream.close();
-            }
+            downloadEqsAsyncTask.execute(new URL("http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson"));
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
         }
-
-        return jsonResponse;
     }
-
-    private String readFromStream(InputStream inputStream) throws IOException {
-        StringBuilder output = new StringBuilder();
-        if (inputStream != null) {
-            InputStreamReader inputStreamReader
-                    = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
-            BufferedReader reader = new BufferedReader(inputStreamReader);
-            String line = reader.readLine();
-            while (line != null) {
-                output.append(line);
-                line = reader.readLine();
-            }
-        }
-
-        return output.toString();
-    }
-
 }
