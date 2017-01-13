605a4f243aed601d02223bf7ebf0cc2e1f4ebf66
Jesus Almaral <almaral17@gmail.com>
2017-01-12 18:02:52 -0700

    Improve DetailActivity UI

--- app/src/main/java/com/almaral/emonitor/DetailActivity.java
+++ app/src/main/java/com/almaral/emonitor/DetailActivity.java
@@ -4,6 +4,10 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 public class DetailActivity extends AppCompatActivity {
 
     @Override
@@ -11,17 +15,28 @@ public class DetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        TextView eqDetailTextView = (TextView) findViewById(R.id.eq_detail_text_view);
-
         Bundle extras = getIntent().getExtras();
         Earthquake earthquake = extras.getParcelable(MainActivity.SELECTED_EARTHQUAKE);
 
         if (earthquake != null) {
-            eqDetailTextView.setText(earthquake.getDateTime() +
-                    "\nMag: " + earthquake.getMagnitude() +
-                    "\nPlace : " + earthquake.getPlace() +
-                    "\nLon: " + earthquake.getLongitude() +
-                    "\nLat: " + earthquake.getLatitude());
+            TextView magnitudeTextView = (TextView) findViewById(R.id.eq_detail_magnitude);
+            TextView longitudeTextView = (TextView) findViewById(R.id.eq_detail_longitude);
+            TextView latitudeTextView = (TextView) findViewById(R.id.eq_detail_latitude);
+            TextView placeTextView = (TextView) findViewById(R.id.eq_detail_place);
+            TextView dateTimeTextView = (TextView) findViewById(R.id.eq_detail_date_time);
+
+            magnitudeTextView.setText(String.valueOf(earthquake.getMagnitude()));
+            longitudeTextView.setText(earthquake.getLongitude());
+            latitudeTextView.setText(earthquake.getLatitude());
+            placeTextView.setText(earthquake.getPlace());
+            dateTimeTextView.setText(getStringDateFromTimestamp(earthquake.getDateTime()));
         }
+    }
+
+    private String getStringDateFromTimestamp(long timestamp) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MMMM/yyyy - H:mm:ss", Locale.getDefault());
+
+        Date date = new Date(timestamp);
+        return simpleDateFormat.format(date);
     }
 }
